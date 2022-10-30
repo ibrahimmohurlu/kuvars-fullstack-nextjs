c640278364174aeea7c2fdbe2c16bc6f875342af
ibrahimmohurlu <ibrahimmohurlu@gmail.com>
2022-10-30 23:02:02 +0300

    MUÄI added to the project. About page implemented.

--- components/About.js
+++ components/About.js
@@ -1,22 +1,47 @@
-import {Tabs} from "flowbite-react";
+import {Grid} from "@mui/material";
 
 export default function About() {
+
     return (
         <section id="about" className="relative md:max-h-screen">
-            {/*heading*/}
-            <div className="flex flex-col md:pt-6 p-8 md:p-12  border-2 border-blue-700 rounded-2xl text-center">
-                <h1 className="text-xl md:text-4xl ">Hakkımızda</h1>
-            </div>
-            {/*About*/}
-            <div className="w-1/3 md:text-xl my-3 mx-4 rounded-xl">
-                <article>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dignissimos magnam neque odit unde. Consequatur ipsam itaque labore quia quis. Ab illo illum minus nam officia pariatur reprehenderit voluptatibus voluptatum?
-                </article>
-                <br/>
-                <article>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae cupiditate eius excepturi, incidunt iusto, natus nisi placeat possimus quas quasi quod reprehenderit, suscipit tempora totam vitae voluptatem. Cum, dolorem.
-                </article>
-            </div>
+            <Grid container spacing={1}>
+                {/*heading*/}
+                <Grid item xs={12}>
+                    <div className="flex flex-col md:pt-6 p-4 md:p-8 rounded-2xl text-center">
+                        <h1 className="text-xl md:text-4xl ">Hakkımızda</h1>
+                    </div>
+                </Grid>
+                {/*About*/}
+                <Grid item xs={5}>
+                    <div className=" md:text-xl my-3 mx-4 rounded-xl">
+                        <div>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dignissimos magnam neque odit
+                            unde.
+                            Consequatur ipsam itaque labore quia quis. Ab illo illum minus nam officia pariatur
+                            reprehenderit voluptatibus voluptatum?
+                        </div>
+                        <div>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dignissimos magnam neque odit
+                            unde.
+                            Consequatur ipsam itaque labore quia quis. Ab illo illum minus nam officia pariatur
+                            reprehenderit voluptatibus voluptatum?
+                        </div>
+                    </div>
+                </Grid>
+                {/*Photo section*/}
+                <Grid item xs={7}>
+                    <div className="relative w-full h-full ">
+                        <div>
+                            <img className="p-2 w-[320px] h-[320px]"
+                                 src="/about-photo.jpg" alt=""/>
+                        </div>
+                        <img className=" mt-[-30%] p-2 float-right  w-[320px] h-[320px]"
+                             src="/carousel-4.jpg" alt=""/>
+                    </div>
+
+
+                </Grid>
+            </Grid>
         </section>
     )
 }
